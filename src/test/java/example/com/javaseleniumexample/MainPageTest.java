de581122b4976df2986e0721c903e4b754d1e507
ExtKadirB <ExtKadirB@ext.d-teknoloji.com.tr>
2024-12-27 22:46:01 +0300

    feat: initial project

---
+++ src/test/java/example/com/javaseleniumexample/MainPageTest.java
@@ -0,0 +1,62 @@
+package example.com.javaseleniumexample;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.chrome.ChromeOptions;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class MainPageTest {
+    MainPage mainPage = new MainPage();
+
+    @BeforeAll
+    public static void setUpAll() {
+        Configuration.browserSize = "1280x800";
+        Configuration.headless = true; // Headless mode
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        Configuration.browserCapabilities = options;
+        open("https://www.jetbrains.com/");
+    }
+
+    @Test
+    public void search() {
+        mainPage.searchButton.click();
+
+        $("[data-test='search-input']").sendKeys("Selenium");
+        $("button[data-test='full-search-button']").click();
+
+        $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
+    }
+
+    @Test
+    public void toolsMenu() {
+        mainPage.toolsMenu.click();
+
+        $("div[data-test='main-submenu']").shouldBe(visible);
+    }
+
+    @Test
+    public void navigationToAllTools() {
+        mainPage.seeDeveloperToolsButton.click();
+        mainPage.findYourToolsButton.click();
+
+        $("#products-page").shouldBe(visible);
+
+        assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());
+    }
+}
